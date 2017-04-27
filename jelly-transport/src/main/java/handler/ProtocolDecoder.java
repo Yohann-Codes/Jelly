e5921fab17713d7f735ee61875fab07579787c3e
Yohann-Codes <yohann_mail@yeah.net>
2017-04-27 10:14:42 +0800

    修改解码器Bug

--- jelly-transport/src/main/java/handler/ProtocolDecoder.java
+++ jelly-transport/src/main/java/handler/ProtocolDecoder.java
@@ -41,13 +41,14 @@ public class ProtocolDecoder extends ByteToMessageDecoder {
         if (in.readableBytes() < ProtocolHeader.HEADER_LENGTH) {
             // 数据包长度小于协议头长度
             logger.info("数据包长度小于协议头长度");
-            ReferenceCountUtil.release(in);
             return;
         }
+        in.markReaderIndex();
+
         if (in.readShort() != ProtocolHeader.MAGIC) {
             // Magic不一致，表明不是自己的数据
             logger.info("Magic不一致");
-            ReferenceCountUtil.release(in);
+            in.resetReaderIndex();
             return;
         }
 
@@ -61,7 +62,7 @@ public class ProtocolDecoder extends ByteToMessageDecoder {
         if (in.readableBytes() != bodyLength) {
             // 消息体长度不一致
             logger.info("消息体长度不一致");
-            ReferenceCountUtil.release(in);
+            in.resetReaderIndex();
             return;
         }
 
